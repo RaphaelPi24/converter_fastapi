939c67f416551db06c8ef255f87e6e4dba6be1bc
RaphaelPi24 <mymojem1@gmail.com>
2025-04-28 22:12:11 +0300

    add video work. no check. need test

--- src/convert_code/video.py
+++ src/convert_code/video.py
@@ -20,7 +20,7 @@ conversion_params = {
 }
 
 
-def convert(input_path: Path, destination_format: str) -> Path:
+def convert(input_path: Path, destination_format: str, output_path: Path|None) -> Path:
     source_format = input_path.suffix.lstrip('.').lower()
     destination_format = destination_format.lower()
 
@@ -30,8 +30,10 @@ def convert(input_path: Path, destination_format: str) -> Path:
         raise ValueError(f"Конвертация из {source_format} в {destination_format} не поддерживается.")
 
     params = conversion_params[source_format][destination_format]
-    if destination_format is None:
+    if output_path is None:
         output_path = input_path.with_suffix(f".{destination_format}")
+    else:
+        output_path = output_path.with_suffix(f".{destination_format}")
 
     command = ["ffmpeg", "-i", str(input_path)] + params + [str(output_path)]
 
@@ -39,35 +41,24 @@ def convert(input_path: Path, destination_format: str) -> Path:
 
     return output_path
 
-
 def mp4_to_avi(input_path: Path, output_path: Path):
-    output_path = output_path.with_suffix(".avi")
-    subprocess.run(["ffmpeg", "-i", str(input_path), str(output_path)], check=True)
-
+    convert(input_path, destination_format="avi", output_path=output_path)
 
 def avi_to_mp4(input_path: Path, output_path: Path):
-    output_path = output_path.with_suffix(".mp4")
-    subprocess.run(["ffmpeg", "-i", str(input_path), str(output_path)], check=True)
-
+    convert(input_path, destination_format="mp4", output_path=output_path)
 
 def mp4_to_webm(input_path: Path, output_path: Path):
-    output_path = output_path.with_suffix(".webm")
-    subprocess.run(["ffmpeg", "-i", str(input_path), "-c:v", "libvpx", "-b:v", "1M", "-c:a", "libvorbis", str(output_path)], check=True)
-
+    convert(input_path, destination_format="webm", output_path=output_path)
 
 def webm_to_mp4(input_path: Path, output_path: Path):
-    output_path = output_path.with_suffix(".mp4")
-    subprocess.run(["ffmpeg", "-i", str(input_path), "-c:v", "libx264", "-c:a", "aac", str(output_path)], check=True)
-
+    convert(input_path, destination_format="mp4", output_path=output_path)
 
 def mov_to_mp4(input_path: Path, output_path: Path):
-    output_path = output_path.with_suffix(".mp4")
-    subprocess.run(["ffmpeg", "-i", str(input_path), "-c:v", "libx264", "-c:a", "aac", str(output_path)], check=True)
-
+    convert(input_path, destination_format="mp4", output_path=output_path)
 
 def mp4_to_mov(input_path: Path, output_path: Path):
-    output_path = output_path.with_suffix(".mov")
-    subprocess.run(["ffmpeg", "-i", str(input_path), "-c:v", "libx264", "-c:a", "aac", str(output_path)], check=True)
+    convert(input_path, destination_format="mov", output_path=output_path)
+
 
 
 """"
