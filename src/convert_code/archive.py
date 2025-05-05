5a9a6a98c13069227510bce2192dc213d335afc9
RaphaelPi24 <mymojem1@gmail.com>
2025-05-05 19:13:52 +0300

    fix bug: add init

--- src/convert_code/archive.py
+++ src/convert_code/archive.py
@@ -14,30 +14,34 @@ class UnsupportedArchiveFormat(ArchiveError):
 
 
 class BaseArchiver:
-    def archive(self, source: Path, destination: Path):
+    def __init__(self, source: Path, destination: Path):
+        self.source = source
+        self.destination = destination
+
+    def archive(self):
         raise NotImplementedError
 
-    def extract(self, source: Path, destination: Path):
+    def extract(self):
         raise NotImplementedError
+
 
 
 class ZipArchiver(BaseArchiver):
-    def archive(self, source: Path, destination: Path):
-        with zipfile.ZipFile(destination, 'w', zipfile.ZIP_DEFLATED) as zipf:
-            for file in source.rglob('*'):
-                zipf.write(file, file.relative_to(source))
+    def archive(self):
+        with zipfile.ZipFile(self.destination, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for file in self.source.rglob('*'):
+                zipf.write(file, file.relative_to(self.source))
 
-    def extract(self, source: Path, destination: Path):
-        with zipfile.ZipFile(source, 'r') as zipf:
-            zipf.extractall(destination)
+    def extract(self):
+        with zipfile.ZipFile(self.source, 'r') as zipf:
+            zipf.extractall(self.destination)
 
 
 class TarGzArchiver(BaseArchiver):
-    def archive(self, source: Path, destination: Path):
-        with tarfile.open(destination, "w:gz") as tar:
-            tar.add(source, arcname=".")
+    def archive(self):
+        with tarfile.open(self.destination, "w:gz") as tar:
+            tar.add(self.source, arcname=".")
 
-    def extract(self, source: Path, destination: Path):
-        with tarfile.open(source, "r:gz") as tar:
-            tar.extractall(path=destination)
-
+    def extract(self):
+        with tarfile.open(self.source, "r:gz") as tar:
+            tar.extractall(path=self.destination)
