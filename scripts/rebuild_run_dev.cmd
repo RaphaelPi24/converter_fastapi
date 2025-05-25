dbaa43da7dc294eccfed686f70ebfae5ce7e20f5
RaphaelPi24 <mymojem1@gmail.com>
2025-05-25 15:34:56 +0300

    correct scripts

--- scripts/rebuild_run_dev.cmd
+++ scripts/rebuild_run_dev.cmd
@@ -1,3 +1,2 @@
 docker compose -f ../build/compose.yml -f ../build/compose.dev.yml down --remove-orphans
-docker compose -f ../build/compose.yml -f ../build/compose.dev.yml build
-docker compose -f ../build/compose.yml -f ../build/compose.dev.yml up --watch
+docker compose -f ../build/compose.yml -f ../build/compose.dev.yml up --build --watch
