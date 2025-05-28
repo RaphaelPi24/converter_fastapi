efcbae7e946398096f36f68766f84c5a3e04d4e3
RaphaelPi24 <mymojem1@gmail.com>
2025-05-28 19:23:42 +0300

    refactor(config): add constants and remove command definitions

--- src/config.py
+++ src/config.py
@@ -6,14 +6,23 @@ from redis import Redis
 from rq import Queue
 
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))  # не работает
 redis_conn = Redis(host=REDIS_HOST, port=REDIS_PORT)
-q = Queue(connection=redis_conn)
+queue_convertation = Queue("converting", connection=redis_conn)
+queue_file_cleanup = Queue("regular_tasks", connection=redis_conn)
 
 BASE_DIR = Path(__file__).resolve().parent
-STATIC_DIR = BASE_DIR / "static"
+STATIC_DIR = BASE_DIR / "templates"
 UPLOAD_DIR = Path("/app/uploaded_files")
 CONVERTED_DIR = Path("/app/converted_files")
-UPLOAD_DIR.mkdir(exist_ok=True)
-CONVERTED_DIR.mkdir(exist_ok=True)
+
 TEMPLATES = Jinja2Templates(directory=STATIC_DIR)
+
+# class Config:
+#     redis: RedisConfig
+#     paths: PathConfig
+#     
+#     
+# config = Config()
+# config.redis.host
+# config.paths.upload
