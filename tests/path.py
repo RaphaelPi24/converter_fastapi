960156d2c7adabb5533bb294f0e639a84c52cdf3
RaphaelPi24 <mymojem1@gmail.com>
2025-07-07 17:55:16 +0300

    add test_selector, path

--- tests/path.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-PROJECT_ROOT = Path(__file__).resolve().parents[1]
-FILES_DIR = PROJECT_ROOT / "tests"
-INPUT_DIR = FILES_DIR / "files before conversion"
-OUTPUT_DIR = FILES_DIR / "files after conversion"
